ea22e3b814e5ef93c7ceb6f6be4faa9760c7daba
kogbonba <kogbonba26@yahoo.co.jp>
2021-04-01 23:07:24 +0900

    first commit

---
+++ insert.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * 1. index.phpのフォームの部分がおかしいので、ここを書き換えて、
+ * insert.phpにPOSTでデータが飛ぶようにしてください。
+ * 2. insert.phpで値を受け取ってください。
+ * 3. 受け取ったデータをバインド変数に与えてください。
+ * 4. index.phpフォームに書き込み、送信を行ってみて、実際にPhpMyAdminを確認してみてください！
+ */
+
+
+
+//1. POSTデータ取得
+//$name = filter_input( INPUT_GET, ","name" ); //こういうのもあるよ
+//$email = filter_input( INPUT_POST, "email" ); //こういうのもあるよ
+
+$name = $_POST["name"];
+$url = $_POST["url"];
+$comment = $_POST["comment"];
+
+
+//2. DB接続します
+try {
+  //ID:'root', Password: 'root'
+  $pdo = new PDO('mysql:dbname=kadai_book;charset=utf8;host=localhost','root','root');
+} catch (PDOException $e) {
+  exit('DBConnectError:'.$e->getMessage());
+}
+
+//３．データ登録SQL作成
+$sql = "INSERT INTO gs_bm_table (id, name, url, comment, indate)
+VALUES(NULL, :a1, :a2, :a3, sysdate())";
+
+
+// 1. SQL文を用意
+$stmt = $pdo->prepare($sql);
+
+//  2. バインド変数を用意
+$stmt->bindValue(':a1', $name, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':a2', $url, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':a3', $comment, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+
+//  3. 実行
+$status = $stmt->execute();
+
+//４．データ登録処理後
+if($status==false){
+  //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+  $error = $stmt->errorInfo();
+  exit("ErrorMessage:". print_r($error, true));
+}else{
+  //５．index.phpへリダイレクト
+  header("Location: select.php");
+  exit;
+
+}
+?>
---
+++ select.php
@@ -0,0 +1,65 @@
+<?php
+require_once('funcs.php');
+//1.  DB接続します
+try {
+    //Password:MAMP='root',XAMPP=''
+    $pdo = new PDO('mysql:dbname=kadai_book;charset=utf8;host=localhost','root','root');
+} catch (PDOException $e) {
+    exit('DBConnectError'.$e->getMessage());
+}
+
+//２．データ取得SQL作成
+$stmt = $pdo->prepare("SELECT * FROM gs_bm_table");
+$status = $stmt->execute();
+
+//３．データ表示
+$view = "";
+if ($status == false) {
+    //execute（SQL実行時にエラーがある場合）
+    $error = $stmt->errorInfo();
+    exit('ErrorQuery:' . print_r($error, true));
+}else{
+    //Selectデータの数だけ自動でループしてくれる
+    //FETCH_ASSOC=http://php.net/manual/ja/pdostatement.fetch.php
+    while( $result = $stmt->fetch(PDO::FETCH_ASSOC)){
+        $view .= '<p>' . h($result['indate']) . ' ' . h($result['name']) . ' ' . h($result['url']) . ' ' . h($result['comment']) . '</p>';
+    }
+
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>お気に入りの本一覧</title>
+<link rel="stylesheet" href="css/range.css">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<style>div{padding: 10px;font-size:16px;}</style>
+</head>
+<body id="main">
+<!-- Head[Start] -->
+<header>
+    <nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+        <a class="navbar-brand" href="index.php">お気に入りの本登録</a>
+        </div>
+    </div>
+    </nav>
+</header>
+<!-- Head[End] -->
+
+<!-- Main[Start] -->
+<div>
+    <div class="container jumbotron">
+    <?php echo $view ?>
+    </div>
+</div>
+<!-- Main[End] -->
+
+</body>
+</html>
